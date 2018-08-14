0c7690d1b8bdf9f1d9fae974152701a21c382110
fabiomasola <masolafabio@gmail.com>
2018-08-14 19:24:42 +0200

    VERSIONE INIZIALE

---
+++ src/it/fabiomasola/boh/bohController.java
@@ -0,0 +1,31 @@
+
+package it.fabiomasola.boh;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class bohController {
+
+    @FXML
+    private TextField txtNome;
+
+   @FXML
+   private Button btnCiao;
+
+    @FXML
+    private TextArea areaSaluto;
+
+    @FXML
+    void handleclick(ActionEvent event) {
+    	System.out.println(txtNome.getText());
+    	
+    	areaSaluto.setText( txtNome.getText());
+    	txtNome.setText("");
+    	
+
+    }
+
+}
